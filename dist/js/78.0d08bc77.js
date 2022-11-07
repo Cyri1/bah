facf4bb9972fbd6c88372db82d1998b7896d8128
Cyri1 <34913372+Cyri1@users.noreply.github.com>
2022-11-07 11:18:14 +0100

    delete dist

--- dist/js/78.0d08bc77.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkbah"]=self["webpackChunkbah"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return a}});var r=n(65),s=n(8487),o=n(6587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,s.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,r.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,s.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.0d08bc77.js.map
