9d3fd382a9d060008a6044cd1d1e8b052721c29d
Cyri1 <cyrilmeliard@gmail.com>
2023-07-25 14:52:38 +0200

    assets

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -35,7 +35,7 @@
             </ion-button>
           </ion-col>
           <ion-col class="ion-align-self-center" size="8">
-            <swiper :loop="true" v-show="storyStore.slidesVisible" :allowTouchMove="!storyStore.isButtonsMode" :modules="modules" :effect="'flip'" @swiper="onSwiper"
+            <swiper :loop="true" v-show="storyStore.slidesVisible" :modules="modules" :effect="'flip'" @swiper="onSwiper"
               @realIndexChange="useReadAudioActiveSlide()">
               <swiper-slide v-for="(slide, index) in storyStore.activeSlides" :key="index">
                 <ion-img @click="storeActiveStoryIndex(index), handleSlideClick(slide.okTransition)"
